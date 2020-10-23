9887b1182c79f5e9a90ea15cdea596a6adfc02f8
Adam Porter <adam@alphapapa.net>
2020-10-23 07:02:10 -0500

    WIP

--- burly-windows.el
+++ burly-windows.el
@@ -105,57 +105,65 @@
     ))
 
 (defun burly-windows--restore-window-configuration (config)
-  (delete-other-windows)
-  (pcase-let* (((map (:frame (map :width :height))
-                     :windows)
-                config)
-               (first-window (cl-loop for window in windows
-                                      when (pcase window
-                                             ((map (:edges `(0 0 ,_ ,_))) t))
-                                      return window))
-               (other-windows (cl-delete first-window windows :test #'equal))
-               (restore-frame-width (frame-width))
-               (restore-frame-height (frame-height)))
-    (cl-assert first-window)
-    ;; Temporarily resize the frame to match the old size.  (I don't
-    ;; know if this will work, but it's worth a try.)
-    (set-frame-width nil width)
-    (set-frame-height nil height)
-    (redisplay)
-
-    ;; Go through the list of saved windows, splitting when an
-    ;; appropriate, existing window is found.  It may be necessary to
-    ;; go through the list more than once before all config are
-    ;; recreated.
-    (cl-loop for i below 100
-             do (when (eq i 99)
-                  (error "Infinite loop detected"))
-             until (zerop (length other-windows))
-             do (cl-loop for saved-window in other-windows
-                         do (pcase-let* (((map (:edges `(,saved-left ,saved-top ,_right ,_bottom))) saved-window))
-                              ;; Find window with same left edge.
-                              (if-let* ((same-left-edge-window (cl-loop for window in (window-list)
-                                                                        for (left top right bottom) = (window-edges window)
-                                                                        when (eq left saved-left)
-                                                                        return window)))
-                                  ;; Found window with same left edge: split vertically (or...horizontally?).
-                                  (with-selected-window same-left-edge-window
-                                    (setf (window-height (split-window-horizontally)) (- bottom top))
-                                    (setf other-windows (cl-delete saved-window other-windows :test #'equal)))
-                                ;; Find window with same top edge.
-                                (if-let* ((same-top-edge-window (cl-loop for window in (window-list)
-                                                                         for (left top right bottom) = (window-edges window)
-                                                                         when (eq top saved-top)
-                                                                         return window)))
-                                    ;; Found window with same top edge: split horizontally (or...vertically?).
-                                    (with-selected-window same-top-edge-window
-                                      (setf (window-width (split-window-vertically)) (- right left))
-                                      (setf other-windows (cl-delete saved-window other-windows :test #'equal))))))))
-    ;; TODO: Reselect selected window.
-    ;; TODO: Restore buffers.
-    (set-frame-width nil restore-frame-width)
-    (set-frame-height nil restore-frame-height)
-    ))
+  (cl-labels ((window-sharing
+               ;; Return the window that shares NUM EDGES.
+               (num edges) (cl-loop for window in (window-list)
+                                    for existing-edges = (window-edges window)
+                                    for num-matching = (cl-loop for i from 0 to 3
+                                                                count (eq (nth i edges) (nth i existing-edges)))
+                                    when (eq num num-matching)
+                                    return window))
+              (split (window new-edges)
+                     ;; Split WINDOW according to which of NEW-EDGES its edges match.
+                     (pcase-let* ((`(,new-left ,new-top ,new-right ,new-bottom) new-edges)
+                                  (`(,left ,top ,right ,bottom) (window-edges window))
+                                  (side (cond ((not (eq new-left left)) 'right)
+                                              ((not (eq new-top top)) 'below)
+                                              ((not (eq new-right right)) 'left)
+                                              ((not (eq new-bottom bottom)) 'above)))
+                                  ;; TODO: Calculate size of new window.
+                                  )
+                       (split-window window nil side))))
+    (delete-other-windows)
+    (pcase-let* (((map (:frame (map :width :height))
+                       :windows)
+                  config)
+                 (frame-width-factor (round (/ (float (frame-width)) (float width))))
+                 (frame-height-factor (round (/ (float (frame-height)) (float height))))
+                 (windows (cl-loop for window in windows
+                                   ;; Avoid modifying the CONFIG passed to this function.
+                                   do (setf window (copy-seq window)
+                                            ;; Normalize window edges in case frame has been resized.
+                                            (map-elt window :edges)
+                                            (pcase-let (((map (:edges `(,left ,top ,right ,bottom))) window))
+                                              (list (* left frame-width-factor)
+                                                    (* top frame-height-factor)
+                                                    (* right frame-width-factor)
+                                                    (* bottom frame-height-factor))))
+                                   collect window))
+                 (first-window (cl-loop for window in windows
+                                        when (pcase window
+                                               ((map (:edges `(0 0 ,_ ,_))) t))
+                                        return window))
+                 (other-windows (cl-delete first-window windows :test #'equal))
+                 (restore-frame-width (frame-width))
+                 (restore-frame-height (frame-height)))
+      (cl-assert first-window)
+      ;; Go through the list of saved windows, splitting when an appropriate, existing window is found.
+      ;; It may be necessary to go through the list more than once before all windows are recreated.
+      (cl-loop for i below 100
+               do (when (eq i 99)
+                    (error "Infinite loop detected"))
+               until (zerop (length other-windows))
+               do (cl-loop for saved-window in other-windows
+                           do (pcase-let* (((map :edges) saved-window)
+                                           (parent-window (window-sharing 2 edges)))
+                                (when parent-window
+                                  (split parent-window edges)
+                                  (setf other-windows (cl-delete saved-window other-windows :test #'equal))))))
+      ;; TODO: Reselect selected window.
+      ;; TODO: Restore buffers.
+      )))
 
 ;;;; Footer
 
