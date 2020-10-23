12055313aedd8e43793e53cf8ecb47605933fe43
Adam Porter <adam@alphapapa.net>
2020-10-23 05:50:45 -0500

    WIP

--- burly-windows.el
+++ burly-windows.el
@@ -53,73 +53,18 @@
                      :windows)
                 config)
                ;; Normalize edges.
-               (frame-width-factor (/ (frame-width) width))
-               (frame-height-factor (/ (frame-height) height))
-               (windows (cl-loop for window in-ref windows
-                                 do (setf (map-elt window :edges)
-                                          (pcase-let (((map (:edges `(,left ,top ,right ,bottom))) window))
-                                            (list (* left frame-width-factor)
-                                                  (* top frame-height-factor)
-                                                  (* right frame-width-factor)
-                                                  (* bottom frame-height-factor))))
-                                 collect window))
-               (first-window (cl-loop for window in windows
-                                      when (pcase window
-                                             ((map (:edges `(0 0 ,_ ,_))) t))
-                                      return window))
-               (other-windows (cl-delete first-window windows :test #'equal)))
-    (cl-assert first-window)
-    ;; TODO: Normalize edges.
-    ;; Go through the list of saved windows, splitting when an
-    ;; appropriate, existing window is found.  It may be necessary to
-    ;; go through the list more than once before all config are
-    ;; recreated.
-    (cl-loop for i below 100          ; Protect against infinite loop.
-             do (when (eq i 99)
-                  (error "Infinite loop detected"))
-             until (zerop (length other-windows))
-             do (cl-loop for saved-window in other-windows
-                         do (pcase-let* (((map (:edges `(,saved-left ,saved-top ,_right ,_bottom))) saved-window))
-                              ;; Find window with same left edge.
-                              (if-let* ((same-left-edge-window (cl-loop for window in (window-list)
-                                                                        for (left top _ _) = (window-edges window)
-                                                                        when (eq left saved-left)
-                                                                        return window)))
-                                  ;; Found window with same left edge: split vertically (or...horizontally?).
-                                  (with-selected-window same-left-edge-window
-                                    (split-window-horizontally)
-                                    (setf other-windows (cl-delete saved-window other-windows :test #'equal)))
-                                ;; Find window with same top edge.
-                                (if-let* ((same-top-edge-window (cl-loop for window in (window-list)
-                                                                         for (left top _ _) = (window-edges window)
-                                                                         when (eq top saved-top)
-                                                                         return window)))
-                                    ;; Found window with same top edge: split horizontally (or...vertically?).
-                                    (with-selected-window same-top-edge-window
-                                      (split-window-vertically)
-                                      (setf other-windows (cl-delete saved-window other-windows :test #'equal))))))))
-    ;; TODO: Reselect selected window.
-    ;; TODO: Restore buffers.
-    ))
-
-(defun burly-windows--restore-window-configuration (config)
-  (delete-other-windows)
-  (pcase-let* (((map (:frame (map :width :height))
-                     :windows)
-                config)
-               ;; Normalize edges.
-               (frame-width-factor (/ (float (frame-width)) (float width)))
-               (frame-height-factor (/ (float (frame-height)) (float height)))
+               (frame-width-factor (round (/ (float (frame-width)) (float width))))
+               (frame-height-factor (round (/ (float (frame-height)) (float height))))
                (windows (cl-loop for window in windows
                                  ;; Avoid modifying the CONFIG passed to this function.
                                  do (setf window (copy-seq window)
                                           ;; Normalize window edges in case frame has been resized.
                                           (map-elt window :edges)
                                           (pcase-let (((map (:edges `(,left ,top ,right ,bottom))) window))
-                                            (list (round (* left frame-width-factor))
-                                                  (round (* top frame-height-factor))
-                                                  (round (* right frame-width-factor))
-                                                  (round (* bottom frame-height-factor)))))
+                                            (list (* left frame-width-factor)
+                                                  (* top frame-height-factor)
+                                                  (* right frame-width-factor)
+                                                  (* bottom frame-height-factor))))
                                  collect window))
                (first-window (cl-loop for window in windows
                                       when (pcase window
@@ -157,6 +102,59 @@
                                       (setf other-windows (cl-delete saved-window other-windows :test #'equal))))))))
     ;; TODO: Reselect selected window.
     ;; TODO: Restore buffers.
+    ))
+
+(defun burly-windows--restore-window-configuration (config)
+  (delete-other-windows)
+  (pcase-let* (((map (:frame (map :width :height))
+                     :windows)
+                config)
+               (first-window (cl-loop for window in windows
+                                      when (pcase window
+                                             ((map (:edges `(0 0 ,_ ,_))) t))
+                                      return window))
+               (other-windows (cl-delete first-window windows :test #'equal))
+               (restore-frame-width (frame-width))
+               (restore-frame-height (frame-height)))
+    (cl-assert first-window)
+    ;; Temporarily resize the frame to match the old size.  (I don't
+    ;; know if this will work, but it's worth a try.)
+    (set-frame-width nil width)
+    (set-frame-height nil height)
+    (redisplay)
+
+    ;; Go through the list of saved windows, splitting when an
+    ;; appropriate, existing window is found.  It may be necessary to
+    ;; go through the list more than once before all config are
+    ;; recreated.
+    (cl-loop for i below 100
+             do (when (eq i 99)
+                  (error "Infinite loop detected"))
+             until (zerop (length other-windows))
+             do (cl-loop for saved-window in other-windows
+                         do (pcase-let* (((map (:edges `(,saved-left ,saved-top ,_right ,_bottom))) saved-window))
+                              ;; Find window with same left edge.
+                              (if-let* ((same-left-edge-window (cl-loop for window in (window-list)
+                                                                        for (left top right bottom) = (window-edges window)
+                                                                        when (eq left saved-left)
+                                                                        return window)))
+                                  ;; Found window with same left edge: split vertically (or...horizontally?).
+                                  (with-selected-window same-left-edge-window
+                                    (setf (window-height (split-window-horizontally)) (- bottom top))
+                                    (setf other-windows (cl-delete saved-window other-windows :test #'equal)))
+                                ;; Find window with same top edge.
+                                (if-let* ((same-top-edge-window (cl-loop for window in (window-list)
+                                                                         for (left top right bottom) = (window-edges window)
+                                                                         when (eq top saved-top)
+                                                                         return window)))
+                                    ;; Found window with same top edge: split horizontally (or...vertically?).
+                                    (with-selected-window same-top-edge-window
+                                      (setf (window-width (split-window-vertically)) (- right left))
+                                      (setf other-windows (cl-delete saved-window other-windows :test #'equal))))))))
+    ;; TODO: Reselect selected window.
+    ;; TODO: Restore buffers.
+    (set-frame-width nil restore-frame-width)
+    (set-frame-height nil restore-frame-height)
     ))
 
 ;;;; Footer
